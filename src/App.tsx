7504837abbf4dab36dd7853f21fc833f3313ed8a
Manotosh12 <monotoshchandrsaha@gmail.com>
2025-06-10 11:29:48 +0530

    tailwind css issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <p> Demo </p>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    </div>
     </>
   )
 }
